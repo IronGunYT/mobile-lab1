b762f77f7e175485ced8a2ff9db8acae84161fb6
IronGun <strannik.kraya575721@gmail.com>
2025-02-16 02:54:07 +0300

    make task 2_2

--- lab1-project2_2_moshkovich/app/src/main/java/com/example/task2_2/MainActivity.kt
+++ lab1-project2_2_moshkovich/app/src/main/java/com/example/task2_2/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.task2_2
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonShow: Button = findViewById(R.id.btnSubscribe)
+        val textViewInfo: TextView = findViewById(R.id.textViewInfo)
+
+        buttonShow.setOnClickListener {
+            if (textViewInfo.visibility == View.VISIBLE) {
+                textViewInfo.visibility = View.GONE
+                buttonShow.text = "Подписаться"
+            } else {
+                textViewInfo.visibility = View.VISIBLE
+                buttonShow.text = "Скрыть"
+            }
         }
     }
 }
